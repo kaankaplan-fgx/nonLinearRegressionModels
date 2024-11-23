313b9ec333f2a203ca548b00aae840f5781a6131
kaankaplan-fgx <75977731+kaankaplan-fgx@users.noreply.github.com>
2024-11-23 22:32:12 +0300

    added xgb

--- nonlinear.R
+++ nonlinear.R
@@ -26,7 +26,7 @@ library(GGally)
 library(pls)
 library(NeuralNetTools)
 library(ISLR)
-
+library(xgboost)
 
 Hitters
 df1 <- Hitters
@@ -247,5 +247,237 @@ plot(cart_model_tuning)
 #bagging
 
 bagging_model <- ipredbagg(education_y, education_x)
+bagging_model
+
+
+education_c <- education %>% dplyr::select(-c("League", "NewLeague", "Division"))
+bagging_model2 <- bagging(Salary~., data= education_c)
+bagging_model2
+
+
+bagging_model_3 <- randomForest(
+  Salary~.,
+  data = education_c,
+  mtry = ncol(education_x), #degisken rastegeleliğini bagging için kaldırıyoruz
+  importance = TRUE, 
+  ntrees=500
+)
+bagging_model_3
+
+predict(bagging_model, test_x)
+predict(bagging_model2, test_x)
+predict(bagging_model_3, test_x)
+
+
+defaultSummary(data.frame(
+  obs=test_y,
+  pred=predict(bagging_model_3, test_x)
+))
+
+plot(bagging_model_3)
+
+bagging_control <- trainControl(
+  method = "cv", number = 10
+)
+bagging_grid <- expand.grid(
+  mtry=ncol(education_x)
+)
+set.seed(3232)
+
+bagging_model_tuning <- train(
+  education_x,
+  education_y,
+  method = "rf", #random forest
+  tuneGrid = bagging_grid,
+  trControl = bagging_control
+)
+bagging_model_tuning
+
+defaultSummary(data.frame(
+  obs=test_y,
+  pred=predict(bagging_model_3, test_x)
+))
+
+#Random Forest
+
+rf_model <- randomForest(education_x, education_y, importance = TRUE)
+importance(rf_model)
+varImpPlot(rf_model)
+predict(rf_model, test_x)
+
+defaultSummary(data.frame(
+  obs=test_y,
+  pred=predict(rf_model, test_x)
+))
+
+
+rf_control <- trainControl(
+  method = "cv", number = 10
+)
+
+rf_grid <- expand.grid(
+  mtry=c(1:16)
+)
+set.seed(3232)
+
+rf_model_tuning <- train(
+  education_x,
+  education_y,
+  method = "rf", #random forest
+  tuneGrid = rf_grid,
+  trControl = rf_control
+)
+
+
+rf_model_tuning
+defaultSummary(data.frame(
+  obs=test_y,
+  pred=predict(rf_model_tuning, test_x)
+))
+
+#BOOSTİNG
+#GBM
+
+gbm_model <- gbm(Salary~. , education_c,
+                distribution = "gaussian",
+                n.trees = 3700,
+                interaction.depth = 1,
+                shrinkage = 0.05,
+                cv.folds = 10
+                )
+
+summary(gbm_model)
+
+defaultSummary(data.frame(
+  obs=education_y,
+  pred=gbm_model$fit
+))
+
+gbm.perf(gbm_model, method = "cv")
+
+defaultSummary(data.frame(
+  obs=test_y,
+  pred=predict(gbm_model, test_x)
+))
+
+gbm_control <- trainControl(
+  method = "cv", number = 10, search = "grid"
+)
+
+gbm_grid <- expand.grid(
+  n.trees = seq(100, 3000, by=100),
+  interaction.depth = seq(1,10, by = 2),
+  shrinkage = seq(0.01, 0.05, by = 0.01),
+  n.minobsinnode = c(10:15)
+)
+set.seed(3232)
+
+gbm_model_tuning <- train(
+  education_x,
+  education_y,
+  method = "gbm", 
+  tuneGrid = gbm_grid,
+  trControl = gbm_control
+)
+plot(gbm_model_tuning)
+
+defaultSummary(data.frame(
+  obs=test_y,
+  pred=predict(gbm_model_tuning, test_x)
+))
+
+
+#XGBoost
+
+xgb_model <- xgboost(data = as.matrix(education_x),
+                     label = education_y,
+                     booster = "gblinear",
+                     max.depth =1,
+                     eta = 0.5,
+                     nrounds = 500,
+                     nthread = 5
+                     )
+
+xgb_education <- xgb.DMatrix(data = as.matrix(education_x), label = education_y)
+xgb_test <- xgb.DMatrix(data = as.matrix(test_x), label = test_y)
+
+xgb_model2 <- xgboost(data = xgb_education,
+                     booster = "gblinear",
+                     max.depth =1,
+                     eta = 0.5,
+                     nrounds = 500,
+                     nthread = 5
+)
+
+xgb.plot.importance(xgb.importance(model = xgb_model))
+
+watchlist  = list(train = xgb_education, test = xgb_test)
+xgb_model3 <- xgb.train(data = xgb_education,
+                      booster = "gblinear",
+                      max.depth =1,
+                      eta = 0.5,
+                      nrounds = 500,
+                      nthread = 5,
+                      watchlist = watchlist
+)
+
+predict(xgb_model3, as.matrix(test_x))
+plot(predict(xgb_model3, as.matrix(test_x)), test_y, xlab = "Tahmin", ylab = "Gerçek", main = "Tahmin - Gerçek", col = "red", pch = 20)
+grid()
+abline(0,1,col = "blue")
+
+xgb_control <- trainControl(
+  method = "cv", number = 10, search = "grid"
+)
+
+xgb_grid <- expand.grid(
+  nrounds = seq(1000, 10000,by= 500),
+  eta = seq(0,1, by = 0.1),
+  lambda = seq(1,5, by=1),
+  alpha = seq(0,1, by = 0.1)
+)
+set.seed(3232)
+
+xgb_model_tuning <- train(
+  x = data.matrix(education_x),
+  y = education_y,
+  method = "xgbLinear", 
+  tuneGrid = xgb_grid,
+  trControl = xgb_control
+) ## çok uzun sürüyor
+
+
+#hyperparameter % tree
+
+xgb_grid_2 <- expand.grid(
+  nrounds = 100,
+  max_depth = 6,
+  eta = 0.3,
+  gamma = 0,
+  colsample_bytree = 1,
+  min_child_weight = 1,
+  subsample = 1
+) #default values from xgb
+
+xgb_control2 <- trainControl(
+  method = "none"
+)
+
+xgb_model4 <- train(
+  x = as.matrix(education_x),
+  y = education_y,
+  tuneGrid = xgb_grid_2,
+  trControl = xgb_control2,
+  method = "xgbTree"
+)
+
+defaultSummary(data.frame(
+  obs=test_y,
+  pred=predict(xgb_model4, as.matrix(test_x))
+))
+
+
+
+
 
 
